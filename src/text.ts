1c75f4f6867e39305613257e0ef1dcf84f9f63bc
Tim Perry <pimterry@gmail.com>
2025-09-15 15:27:15 +0200

    Extend joinAnd with oxfordComma option

--- src/text.ts
+++ src/text.ts
@@ -6,10 +6,24 @@ export function truncateWithEllipsis(str: string, length: number) {
     }
 }
 
-export function joinAnd(val: string[], initialSep = ', ', finalSep = ' and ') {
+export function joinAnd(val: string[], options: {
+    // Separator between all but the last value
+    separator?: string,
+    // Separator before last value (preceeded with space or `separator` if oxfordComma is true)
+    finalSeparator?: string,
+    // Should there be a separator between the penultimate value & finalSeparator?
+    oxfordComma?: boolean
+} = {}) {
     if (val.length === 1) return val[0];
 
-    return val.slice(0, -1).join(initialSep) + finalSep + val[val.length - 1];
+    const separator = options.separator ?? ', ';
+    const finalSeparator = options.finalSeparator ?? 'and ';
+    const oxfordComma = options.oxfordComma ?? false;
+
+    return val.slice(0, -1).join(separator) +
+        (oxfordComma ? separator : ' ') +
+        finalSeparator +
+        val[val.length - 1];
 }
 
 const VOWEL_ISH = ['a', 'e', 'i', 'o', 'u', 'y'];
